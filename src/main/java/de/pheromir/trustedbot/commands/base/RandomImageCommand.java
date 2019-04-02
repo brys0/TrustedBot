23d2c9d712f8d87d48b3005bca1268f913d2deb2
Unknown <Pheromir@hotmail.de>
2019-04-02 14:20:32 +0200

    Adjusted RandomImage to TrustedCommand and using async methods

--- src/main/java/de/pheromir/trustedbot/commands/base/RandomImageCommand.java
+++ src/main/java/de/pheromir/trustedbot/commands/base/RandomImageCommand.java
@@ -3,9 +3,13 @@ package de.pheromir.trustedbot.commands.base;
 import java.awt.Color;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import de.pheromir.trustedbot.Main;
-import de.pheromir.trustedbot.Methods;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.ChannelType;
@@ -27,14 +31,39 @@ public abstract class RandomImageCommand extends TrustedCommand {
 
 	@Override
 	protected boolean exec(CommandEvent e) {
-		String imgUrl;
 		try {
-			imgUrl = Methods.httpRequestJSON(BASE_URL).getString(jsonKey);
-			e.reply(new EmbedBuilder().setImage(imgUrl).setColor(e.getChannelType() == ChannelType.TEXT
-					? e.getSelfMember().getColor()
-					: Color.BLUE).build());
+			Unirest.get(BASE_URL).asJsonAsync(new Callback<JsonNode>() {
+
+				@Override
+				public void completed(HttpResponse<JsonNode> response) {
+					if(response.getStatus() != 200) {
+						Main.LOG.error("RandomImage " + RandomImageCommand.this.name + " received HTTP Code " + response.getStatus());
+						e.reply("An error occurred while getting a random Image");
+						return;
+					}
+					String imgUrl = response.getBody().getObject().getString(jsonKey);
+					e.reply(new EmbedBuilder().setImage(imgUrl).setColor(e.getChannelType() == ChannelType.TEXT
+							? e.getSelfMember().getColor()
+							: Color.BLUE).build());
+				}
+
+				@Override
+				public void failed(UnirestException e1) {
+					Main.LOG.error("Getting RandomImage " + RandomImageCommand.this.name + " cancelled.");
+					e.reply("An error occurred while getting a random Image");
+				}
+
+				@Override
+				public void cancelled() {
+					Main.LOG.error("Getting RandomImage " + RandomImageCommand.this.name + " cancelled.");
+					e.reply("An error occurred while getting a random Image");
+				}
+				
+			});
+			return true;
 		} catch (Exception e1) {
 			Main.LOG.error("", e1);
+			return false;
 		}
 	}
 
