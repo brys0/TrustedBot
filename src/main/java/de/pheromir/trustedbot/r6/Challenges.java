213b63734c658a9c714b9e9d429991da377ad2a2
MeFisto94 <MeFisto94@users.noreply.github.com>
2020-04-12 23:49:50 +0200

    Fix Challenges not honoring "allOperators" when a filter is present

--- src/main/java/de/pheromir/trustedbot/r6/Challenges.java
+++ src/main/java/de/pheromir/trustedbot/r6/Challenges.java
@@ -52,11 +52,11 @@ public enum Challenges {
     RUSH(op -> Arrays.asList(op.specialAbilities).contains(SpecialAbilities.RUSH), "Rush", "Rush B сука блять\n(Dont stop walking!)"),
     SPAWN_PEEK(op -> Arrays.asList(op.specialAbilities).contains(SpecialAbilities.SPAWN_PEEK), "Spawn peek", "Peek the hell out of the attackers"),
     MEMES(op -> Arrays.asList(op.specialAbilities).contains(SpecialAbilities.MEME), "Meme", "Play \"Meme-operators\""),
-    REVERSE(op -> op.side == Side.DEFENDERS, "Reverse Role", "Roamers are anchors, anchors are roamers"),
-    NO_REINFORCEMENTS(op -> op.side == Side.DEFENDERS, "No Reinforcements", "You're not allowed to use any reinforcement"),
-    ONE_REINFORCEMENT(op -> op.side == Side.DEFENDERS, "One Reinforcement", "You're only allowed to use one reinforcement each"),
+    REVERSE(op -> op.side == Side.DEFENDERS, "Reverse Role", "Roamers are anchors, anchors are roamers", true),
+    NO_REINFORCEMENTS(op -> op.side == Side.DEFENDERS, "No Reinforcements", "You're not allowed to use any reinforcement", true),
+    ONE_REINFORCEMENT(op -> op.side == Side.DEFENDERS, "One Reinforcement", "You're only allowed to use one reinforcement each", true),
     NO_GADGETS("No Gadgets", "You're not allowed to use your primary-/secondary gadgets"),
-    ONE_AT_A_TIME(op -> op.side == Side.ATTACKERS, "One at a time", "Only one person at the same time in the building. The last one in the scoreboard starts, then the one above (and so on..)"),
+    ONE_AT_A_TIME(op -> op.side == Side.ATTACKERS, "One at a time", "Only one person at the same time in the building. The last one in the scoreboard starts, then the one above (and so on..)", true),
     
     // Misc
     TOXIC(op -> Arrays.asList(op.specialAbilities).contains(SpecialAbilities.TOXIC), "Toxic", "Play operators that are considered as toxic"),
@@ -90,21 +90,39 @@ public enum Challenges {
     /**
      * Constructs a challenge which is only completable with Operators passing the filter.
      *
-     * @param filter The predicated used to filter capable operators
+     * @param filter The predicate used to filter capable operators
      * @param name The canonical name of this challenge
      * @param desc The description of this challenge.
      */
     Challenges(Predicate<Operator> filter, String name, String desc) {
+        this(filter, name, desc, false);
+    }
+
+    /**
+     * Constructs a challenge which is completable with all Operators
+     * @param name The canonical name of this challenge
+     * @param desc The description of this challenge
+     */
+    Challenges(String name, String desc) {
+        this(((Operator op) -> true), name, desc, true);
+    }
+
+    /**
+     * Constructs a challenge which is completable by "ALL" (allOperators) passing the filter.<br />
+     * This is used for Challenges where a Predicate might filter for {@link Side} but still technically allow for all
+     * Operators. Thus one can specify allOperators to indicate that the individual Operators aren't printed to the
+     * message, even though some Operators might be filtered.
+     *
+     * @param filter The predicate used to filter capable operators
+     * @param name The canonical name of this challenge
+     * @param desc The description of this challenge
+     * @param allOperators Whether this Challenge is completable on "ALL" Operators, regardless of the filter this is
+     *                     just a Hint for the UI to NOT print out all individual operators.
+     */
+    Challenges(Predicate<Operator> filter, String name, String desc, boolean allOperators) {
         this.filter = filter;
         this.name = name;
         this.desc = desc;
-        this.allOperators = false;
-    }
-
-    Challenges(String name, String desc) {
-    	this.filter = ((Operator op) -> true);
-        this.name = name;
-        this.desc = desc;
-        this.allOperators = true;
+        this.allOperators = allOperators;
     }
 }
